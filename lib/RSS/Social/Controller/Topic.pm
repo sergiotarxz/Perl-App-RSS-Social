49d6e80272d9ebded4b4485c402ff2a78f064529
Sergio <sergiotarxz@posteo.net>
2026-01-03 16:37:07 +0100

    Adding new topic menu.

--- lib/RSS/Social/Controller/Topic.pm
+++ lib/RSS/Social/Controller/Topic.pm
@@ -9,22 +9,58 @@ use RSS::Social::Topic;
 use RSS::Social::Messages;
 use RSS::Social::User;
 use RSS::Social::Controller::Log;
+use UUID qw/uuid4/;
 
 sub visit {
-	my ($self)     = @_;
-	my $slug = $self->param('slug');
-	my ($topic) = @{RSS::Social::Topic->search(
-		slug => $slug,
-	)};
-	my @messages = @{RSS::Social::Messages->free_search(
-		-where => {
-			'messages.id_topic' => $topic->id,
-		},
-		-limit => 30,
-		-order_by => 'messages.created DESC',
-	)};
-	$self->render(topic => $topic, messages => \@messages);
+    my ($self)  = @_;
+    my $slug    = $self->param('slug');
+    my ($topic) = @{ RSS::Social::Topic->search(
+            slug => $slug,
+        )
+    };
+    my @messages = @{ RSS::Social::Messages->free_search(
+            -where => {
+                'messages.id_topic' => $topic->id,
+            },
+            -limit    => 30,
+            -order_by => 'messages.created DESC',
+        )
+    };
+    $self->render( topic => $topic, messages => \@messages );
 }
 
-RSS::Social::Controller::Log->import(qw/visit/);
+sub get_create_topic {
+    my $self = shift;
+    $self->render;
+}
+
+sub post_create_topic {
+    my $self              = shift;
+    my $topic_name        = $self->param('topic_name');
+    my $topic_slug        = $self->param('topic_slug');
+    my $topic_description = $self->param('topic_description');
+    if ( $topic_slug !~ /^(?:[a-zA-Z0-9_]|-){5,}$/ ) {
+        say 'slug';
+        return $self->redirect_to('/private/topic/create');
+    }
+    if ( 5 > length $topic_name ) {
+        say 'name';
+        return $self->redirect_to('/private/topic/create');
+    }
+    if ( 5 > length $topic_description ) {
+        say 'description';
+        return $self->redirect_to('/private/topic/create');
+    }
+    my $uuid  = uuid4();
+    my $topic = RSS::Social::Topic::Instance->new(
+        uuid        => $uuid,
+        name        => $topic_name,
+        slug        => $topic_slug,
+        description => $topic_description,
+    );
+    RSS::Social::Topic->insert($topic);
+    return $self->redirect_to("/rs/@{[$topic->slug]}");
+}
+
+RSS::Social::Controller::Log->import(qw/visit get_create_topic/);
 1;
